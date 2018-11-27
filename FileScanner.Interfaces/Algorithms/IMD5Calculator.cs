8b4c6c791ca04728102b78c4aaeb89d3a3baba23
Andrew Olden <Andrew.Olden@outlook.com>
2018-11-27 19:20:11 +0000

    Basic Interface

---
+++ FileScanner.Interfaces/Algorithms/IMD5Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScanner.Interfaces
+{
+    /// <summary>
+    /// Methods used to calculate the MD5 hash of file.
+    /// </summary>
+    public interface IMD5Calculator
+    {
+        string CalculateHash(string Path);
+    }
+}
