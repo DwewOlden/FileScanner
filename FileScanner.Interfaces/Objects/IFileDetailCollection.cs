fd6c39b51ed04a5b9d966fd7bdf57dfd2687e2c2
Andrew Olden <Andrew.Olden@outlook.com>
2018-11-27 19:28:30 +0000

    More interface work

---
+++ FileScanner.Interfaces/Objects/IFileDetailCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScanner.Interfaces.Objects
+{
+    public interface IFileDetailCollection
+    {
+        bool FileHasBeenScanned { get; set; }
+
+
+    }
+}
---
+++ FileScanner.Interfaces/Objects/IFileDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScanner.Interfaces.Objects
+{
+    /// <summary>
+    /// The path and hash of a file
+    /// </summary>
+    public interface IFileDetails
+    {
+        string Path { get; set; }
+
+        string Hash { get; set; }
+    }
+}
