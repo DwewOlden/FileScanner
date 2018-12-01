2a708063d3c0f018986de2b518f486ea509415fe
Andrew Olden <Andrew.Olden@outlook.com>
2018-12-01 16:00:02 +0000

    The system should now be initalizing correctly.

--- FileScanner.Algorithms/ProcessingSystem.cs
+++ FileScanner.Algorithms/ProcessingSystem.cs
@@ -1,5 +1,7 @@
 ﻿using FileScanner.Interfaces.Algorithms;
+using FileScanner.Interfaces.IO;
 using FileScanner.Interfaces.Objects;
+using FileScanner.Objects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,12 +16,31 @@ namespace FileScanner.Algorithms
     public class ProcessingSystem : IProcessingSystem
     {
 
-        public ProcessingSystem()
-        {
+        #region Constructors
 
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public ProcessingSystem(IScanningLocations scanningLocations)
+        {
+            scanningLocations_ = scanningLocations;
         }
 
+        #endregion
 
+        #region Variables
+
+        /// <summary>
+        /// Loads the data files and collections
+        /// </summary>
+        private readonly IScanningLocations scanningLocations_;
+
+
+        #endregion
+
+        #region Properties
+
+        public IPathProperties PathProperties { get; set; }
 
         /// <summary>
         /// Details of any files that have been added to the system.
@@ -36,13 +57,45 @@ namespace FileScanner.Algorithms
         /// </summary>
         public IEnumerable<string> RemovedFiles { get; set; }
 
+        #endregion
+
+        #region Processors
+
+       
+
+        #endregion
+
         /// <summary>
         /// Main entry point into the system.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>True if the process has a successfull run</returns>
         public bool ProcessFileDetails()
         {
-            throw new NotImplementedException();
+            if (PathProperties == null)
+                throw new ArgumentOutOfRangeException("PathProperties", "the path properties object is currently null");
+
+            if (!InitalizeCollections())
+                return false;
+
+            
+
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the informations from the files.
+        /// </summary>
+        /// <remarks>The paths to the files are specified in the PathProperties object</remarks>
+        private bool InitalizeCollections()
+        {
+            if (!scanningLocations_.Populate(PathProperties))
+                throw new ArgumentNullException("PathProperties", "unable to load the contents of the path files");
+
+            if (scanningLocations_.FileDetails.FileCount != 0 && scanningLocations_.Directories.Count() != 0)
+                return true;
+
+            return false;
+
         }
     }
 }
--- FileScanner.Unity/Bootstrapper.cs
+++ FileScanner.Unity/Bootstrapper.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿using FileScanner.Algorithms;
+using FileScanner.Interfaces;
+using FileScanner.Interfaces.Algorithms;
+using FileScanner.Interfaces.IO;
+using FileScanner.Interfaces.Objects;
+using FileScanner.IO;
+using FileScanner.Objects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity;
 
 namespace FileScanner.Unity
 {
@@ -11,9 +19,50 @@ namespace FileScanner.Unity
     /// </summary>
     public static class Bootstrapper
     {
+        public static readonly UnityContainer UnityContainer = new UnityContainer();
+
+        /// <summary>
+        /// Registers the interfaces with the implemention. 
+        /// </summary>
+        /// <remarks>
+        /// split into sections for readability only,
+        /// </remarks>
         public static void InitalizeUnity()
         {
-
+            RegisterProcessors();
+            RegisterIOHelpers();
+            RegisterObjects();
+            
         }
+
+        /// <summary>
+        /// Register the IO helpers associated with the system
+        /// </summary>
+        private static void RegisterIOHelpers()
+        {
+            UnityContainer.RegisterType<IFileLoader, FileLoader>();
+        }
+
+        /// <summary>
+        /// Register the support objects.
+        /// </summary>
+        private static void RegisterObjects()
+        {
+            UnityContainer.RegisterType<IFileDetailCollection, FileDetailCollection>();
+            UnityContainer.RegisterType<IFileDetails, FileDetails>();
+            UnityContainer.RegisterType<IPathProperties, PathProperties>();
+            UnityContainer.RegisterType<IScanningLocations, ScanningLocations>();
+        }
+        
+        /// <summary>
+        /// Register the processors
+        /// </summary>
+        private static void RegisterProcessors()
+        {
+            UnityContainer.RegisterType<IDirectoryScanner, DirectoryScanner>();
+            UnityContainer.RegisterType<IProcessingSystem, ProcessingSystem>();
+            UnityContainer.RegisterType<IMD5Calculator, MD5Calculator>();
+        }
+
     }
 }
